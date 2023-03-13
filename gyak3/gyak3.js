b07b72cf653db99bbfbb4a5a2fc657db0ebc461c
bbarn3y <babuafi@inf.elte.hu>
2023-03-13 12:38:34 +0100

    3. gyakorlat WIP

--- gyak3/gyak3.js
+++ gyak3/gyak3.js
@@ -1,6 +1,15 @@
 const colorMeEl = document.querySelector('#colorMe');
 const changeColorEl = document.getElementById('coloringButton');
 
+const colors = ['blue', 'green', 'red', 'black'];
 changeColorEl.addEventListener('click', (event) => {
-    console.log('event', event);
+    console.log('event', event, colorMeEl.style.color);
+    const currentColorIndex = colors
+        .findIndex((color) => color === colorMeEl.style.color);
+    const newColor = (currentColorIndex === -1 || currentColorIndex + 1 >= colors.length)
+        ? colors[0]
+        : colors[currentColorIndex + 1];
+    colorMeEl.style.color = newColor;
 });
+
+
