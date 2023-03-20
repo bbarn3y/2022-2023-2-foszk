61c57dfde8647ac07e95f35ab875ee33f6f15021
bbarn3y <babuafi@inf.elte.hu>
2023-03-20 12:04:20 +0100

    ZH1

---
+++ zh1/zh1.js
@@ -0,0 +1,98 @@
+let students = [
+    {
+        name: "Ludvig Patrik",
+        score: 10,
+        start: 2021,
+        favouriteTask: 2
+    },
+    {
+        name: "Bikás Boldizsár",
+        score: 10,
+        start: 2017,
+        favouriteTask: 2
+    },
+    {
+        name: "Kamrás Ádám Botond",
+        score: 10,
+        start: 2019,
+        favouriteTask: 1
+    },
+    {
+        name: "Kovács Krisztián Emil",
+        score: 10,
+        start: 2022,
+        favouriteTask: 1
+    },{
+        name: "Kóródi Gergő",
+        score: 10,
+        start: 2017,
+        favouriteTask: 1
+    },
+    {
+        name: "Kemenesi Máté",
+        score: 10,
+        start: 2018,
+        favouriteTask: 1
+    },
+    {
+        name: "Fekete Kíra",
+        score: 10,
+        start: 2021,
+        favouriteTask: 2
+    },
+    {
+        name: "Kristóf Áron Zsolt",
+        score: 10,
+        start: 2020,
+        favouriteTask: 2
+    },
+    {
+        name: "Ökrös Bence László",
+        score: 10,
+        start: 2019,
+        favouriteTask: 1
+    },
+    {
+        name: "Vörös András István",
+        score: 10,
+        start: 2022,
+        favouriteTask: 1
+    },
+    {
+        name: "Nagy Vencel",
+        score: 10,
+        start: 2018,
+        favouriteTask: 1
+    },
+    {
+        name: "Burda Botond Ádám",
+        score: 10,
+        start: 2021,
+        favouriteTask: 1
+    }
+]
+
+/**
+ * Task 1: Array
+ */
+
+// 1.1
+
+
+// 1.2
+
+
+// 1.3
+
+
+// 1.4
+
+
+// 1.5
+
+
+
+
+/**
+ * Task 2: Color selector
+ */
