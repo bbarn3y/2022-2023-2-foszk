dc2e250e952487a0e18ad1b68f9ea196d26c441d
bbarn3y <babuafi@inf.elte.hu>
2023-03-27 13:32:38 +0200

    5. gyakorlat

--- gyak5/gyak5.js
+++ gyak5/gyak5.js
@@ -21,22 +21,63 @@ console.log('solution', solution);
 const containerEl = document.getElementById('container');
 
 function adversaryGuess() {
+    const availableNumbers = [...document.querySelectorAll('.cell')]
+        .filter((cellEl) => cellAvailable(cellEl))
+        .map((cellEl) => +cellEl.textContent);
 
+    console.log('availableNumbers', availableNumbers);
+
+    const adversarysGuess = availableNumbers[randomNumber(0, availableNumbers.length - 1)]
+
+    console.log('adversarysGuess', adversarysGuess)
 }
 
-function cellAvailable(target) {
-
+function cellAvailable(cell) {
+    return !cell.classList.contains('player-wins') &&
+        !cell.classList.contains('player-wrong-guess')
 }
 
 function createGrid() {
+    function createRow(rowId) {
+        return [...new Array(10).keys()]
+            .map(i => i + 1)
+            .map((colNumber) => `<div class="cell">${rowId * 10 + colNumber}</div>`)
+            .join('');
+    }
 
+    const html = [...new Array(10).keys()].map((rowNumber) => {
+        return createRow(rowNumber);
+    }).join('<br>');
+
+    // console.log(html);
+
+    containerEl.innerHTML = html;
 }
 
 createGrid();
 
-// delegate(, '', '', (event) => {
-//
-// });
+delegate(containerEl, 'click', '.cell',  function (event) {
+    // console.log(this, event);
+    if (!cellAvailable(this) || gameOver) {
+        return;
+    }
+
+    const guess = +this.textContent;
+
+    console.log('guess', guess);
+
+    if (solution === guess) {
+        this.classList.add('player-wins');
+        gameOver = true;
+        return;
+    }
+
+    this.classList.add('player-wrong-guess');
+});
+
+window.setInterval(() => {
+    adversaryGuess();
+}, 5000);
 
 
 
