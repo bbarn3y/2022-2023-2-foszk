ac39a98cbf0fabb50151e5d03d22ed62b6c4c974
bbarn3y <babuafi@inf.elte.hu>
2023-04-03 13:13:08 +0200

    6. gyakorlat [WIP]

--- gyak6/gyak6.js
+++ gyak6/gyak6.js
@@ -24,6 +24,25 @@ class Bird {
     }
 }
 
+class Column {
+    constructor(context, x, y, width, height) {
+        this.context = context;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw() {
+        context.fillStyle = 'green';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update(dt) {
+        this.x -= 125 * dt / 1000
+    }
+}
+
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
         bird.velocity -= 100;
@@ -38,6 +57,20 @@ const canvas = document.getElementById('flappyCanvas');
 const context = canvas.getContext('2d');
 
 const bird = new Bird(context, 50, 200, 50, 50, 10, 100);
+const columns = [];
+
+const columnGap = 200;
+const columnDistance = 250;
+
+function createColumns() {
+    const topColumnHeight = random(75, 150);
+    columns.push(...[
+        new Column(context, canvas.width, 0, 40, topColumnHeight),
+        new Column(context, canvas.width, topColumnHeight + columnGap, 40, canvas.height - topColumnHeight- columnGap)
+    ]);
+}
+
+createColumns();
 
 let previousTime = performance.now();
 function cycle(now = performance.now()) {
@@ -65,10 +98,18 @@ function draw() {
     // context.stroke();
 
     bird.draw();
+
+    columns.forEach((column) => {
+        column.draw();
+    })
 }
 
 function update(dt) {
     bird.update(dt);
+
+    columns.forEach((column) => {
+        column.update(dt);
+    })
 }
 
 cycle();
