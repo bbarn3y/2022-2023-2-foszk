906fe0acec81c093c53d8b2d002132e37f474fef
bbarn3y <babuafi@inf.elte.hu>
2023-04-03 12:34:42 +0200

    6. gyakorlat [WIP]

--- gyak6/gyak6.js
+++ gyak6/gyak6.js
@@ -1,4 +1,46 @@
+class Bird {
+    constructor(context, x, y, width, height, velocity, acceleration) {
+        this.context = context
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+    }
+
+    draw() {
+        context.fillStyle = 'gray';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
 function random(a, b) {
     return Math.floor(Math.random() * (b - a + 1)) + a;
 }
+
+const canvas = document.getElementById('flappyCanvas');
+const context = canvas.getContext('2d');
+
+const bird = new Bird(context, 50, 200, 50, 50, 10, 100);
+
+function draw() {
+    console.log('draw');
+    context.fillStyle = 'aqua';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // context.clearRect(100, 100, 50, 50);
+
+    // context.beginPath();
+    // context.moveTo(200, 200);
+    // context.lineTo(225, 250);
+    // context.lineTo(250, 200);
+    // context.lineTo(200, 200);
+    // context.closePath();
+    // context.stroke();
+
+    bird.draw();
+}
+
+draw();
 
