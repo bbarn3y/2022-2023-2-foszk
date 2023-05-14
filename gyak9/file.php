ce46c97ec69e8e6dca562ae918a1076337e10e2b
bbarn3y <babuafi@inf.elte.hu>
2023-05-14 09:07:24 +0200

    9. gyakorlat

--- gyak9/file.php
+++ gyak9/file.php
@@ -10,3 +10,20 @@ print_r($parsedPersons);
 fclose($personsFile);
 
 ?>
+
+<?php
+$adultPersons = array_filter($parsedPersons, fn($person) => $person['age'] >= 18);
+?>
+<ul>
+<?php foreach ($adultPersons as $adultPerson): ?>
+    <li><?= $adultPerson['name'] ?></li>
+<?php endforeach; ?>
+</ul>
+
+<?php
+$adultPersonsFile = fopen('adult_persons.csv', 'w');
+fwrite($adultPersonsFile, 'name,age,sex' . PHP_EOL);
+foreach ($adultPersons as $adultPerson) {
+    fwrite($adultPersonsFile, $adultPerson['name'] . ',' . $adultPerson['age'] . ',' . $adultPerson['sex'] . PHP_EOL);
+}
+fclose($adultPersonsFile);
